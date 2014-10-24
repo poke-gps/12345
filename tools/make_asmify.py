9380bcb29d4d41b213b725b98f11681c94c51a89
Alon Zakai <alonzakai@gmail.com>
2014-10-23 17:13:32 -0700

    work

--- tools/make_asmify.py
+++ tools/make_asmify.py
@@ -20,27 +20,31 @@ emterpreter = temp_js[start:end+2]
 
 # process emterpreter: convert runnable emterpreter in JS into something that processes bytecode into JS
 def magic(code):
+  # this is super-hackish, but the emterpreter is so small, it is better to do this than to write a translator from scratch (there are over 100 opcodes...)
   code = code[code.index('{')+2:-2] # strip out function name and outer {}
   code = code.replace('pc = pc | 0;', '', 1) # unsigned char *base_pc = pc;\n int *HEAP32 = (int*)pc;', 1)
   code = code.replace('var sp = 0, inst = 0, lx = 0, ly = 0, lz = 0;', 'int inst; unsigned char lx, ly, lz;', 1)
   code = code.replace('sp = EMTSTACKTOP;', '', 1)
-  code = code.replace('HEAPU8[pc + 1 >> 0] | 0', 'pc[1]')
-  code = code.replace('HEAPU8[pc + 2 >> 0] | 0', 'pc[2]')
-  code = code.replace('HEAPU8[pc + 4 >> 0] | 0', 'pc[4]')
+  code = code.replace('HEAPU8[pc + 1 >> 0] | 0', 'pc[1]', 1)
+  code = code.replace('HEAPU8[pc + 2 >> 0] | 0', 'pc[2]', 1)
+  code = code.replace('HEAPU8[pc + 4 >> 0] | 0', 'pc[4]', 1)
   code = code.replace('EMTSTACKTOP = EMTSTACKTOP + (lx << 3) | 0;', '', 1)
-  code = code.replace('(ly | 0)', 'ly')
-  code = code.replace('(lz | 0)', 'lz')
+  code = code.replace('(ly | 0)', 'ly', 1)
+  code = code.replace('(lz | 0)', 'lz', 1)
   code = code.replace('HEAPF64[sp + (ly << 3) >> 3] = ', '//', 1)
-  code = code.replace(' + 4 | 0', ' + 4')
+  code = code.replace(' + 4 | 0', ' + 4', 1)
   code = code.replace('HEAP32[pc >> 2] | 0', '*((int*)pc)', 1)
-  code = code.replace('inst >> 8 & 255', '(unsigned char)(inst >> 8)')
-  code = code.replace('inst >> 16 & 255', '(unsigned char)(inst >> 16)')
-  code = code.replace('inst >>> 24', '(unsigned char)(inst >> 24)')
+  code = code.replace('inst >> 8 & 255', '(unsigned char)(inst >> 8)', 1)
+  code = code.replace('inst >> 16 & 255', '(unsigned char)(inst >> 16)', 1)
+  code = code.replace('inst >>> 24', '(unsigned char)(inst >> 24)', 1)
   code = code.replace('inst & 255', '(unsigned char)inst', 1)
-  code = code.replace('', '', 1)
-  code = code.replace('', '', 1)
-  code = code.replace('', '', 1)
-  code = code.replace('', '', 1)
+  # the main switch is the core code generation routine. each case has some code which runs in the emterpreter, and which we instead *emit*, here, after some transformations
+  #for var in ['lx', 'ly', 'lz']:
+  #  for suffix in [' | 0', '']:
+  #    code = code.replace('HEAP32[sp + (' + var + ' << 3) >> 2]' + suffix, var)
+  #  code = code.replace('(' + var + ')', var)
+  #code = code.replace(' | 0;', ';', 1)
+  #code = code.replace('', '', 1)
 
 
 
