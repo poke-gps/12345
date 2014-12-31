f6de3c0e6a5b32dd62f119740c2d6546d71dbb68
Alon Zakai <alonzakai@gmail.com>
2014-12-31 10:13:06 -0800

    followup: update other.test_simd

--- tests/test_other.py
+++ tests/test_other.py
@@ -2429,7 +2429,7 @@ int main()
       self.assertContained('File size: 724', out)
 
   def test_simd(self):
-    assert get_clang_version() == '3.4'
+    assert get_clang_version() == '3.5'
     Popen([PYTHON, EMCC, path_from_root('tests', 'linpack.c'), '-O2', '-s', 'SIMD=1', '-DSP', '-s', 'PRECISE_F32=1']).communicate()
     self.assertContained('Unrolled Single  Precision', run_js('a.out.js'))
 
